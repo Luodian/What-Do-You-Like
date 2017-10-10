58041bd02fb9327926aac8437cb1513744496e4c
Luodian <1121058986@qq.com>
2017-10-10 16:40:11 +0800

    modified password to invis

--- 9/src/Database/DataBaseConnector.java
+++ 9/src/Database/DataBaseConnector.java
@@ -10,9 +10,8 @@ public class DataBaseConnector {
 
     // JDBC 驱动名及数据库 URL
     static final String url = "jdbc:mysql://127.0.0.1/books?characterEncoding=utf8";
-    static final String connectionString = "jdbc:mysql://47.95.194.146:3306/books?user=root&password=luodian&useUnicode=true&characterEncoding=utf8";
     static final String user = "root";
-    static final String password = "luodian";
+    static final String password = "*******";
 
     public Connection connect;
     public Statement sta = null;
